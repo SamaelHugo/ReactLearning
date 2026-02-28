ec32714d095ea8a1df65f40721ded02b9eb9b0ad
SamaelHugo <aubekirov.2@gmail.com>
2026-02-28 22:09:13 +0500

    добавлять убирать комплитить

--- src/App.js
+++ src/App.js
@@ -3,46 +3,39 @@ import {useState} from "react";
 import {Task} from "./task";
 
 function App() {
-    const [todoList, setTodoList] = useState([]); // стейт, создает список заданий называется todoList,
-    // чтобы его менять создается сеттудулист
-    const [newTask, setNewTask] = useState(""); // само задание, называется ньютаск это строка,
-    // чтобы взаимодействовать используется сетнью таск
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState("");
 
-    const handleChange = (e) => { // функция, для изменения значения задания
+    const handleChange = (e) => {
         setNewTask(e.target.value);
     };
 
-    const addTask = () => { // функция добавления задания
-        const task = { // само задание это объект с 3 параметрами
-            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1, // айдм, чтобы понимать какой номер.
-            // берется размер всего листа, если 0, то задание 1, если нет, то у последнего задания берется айди и добавляется 1
-            taskName: newTask, // само задание которое из стейта
-            isCompleted: false // по базе фолс
+    const addTask = () => {
+        const task = {
+            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+            taskName: newTask,
+            isCompleted: false
         }
-      setTodoList([...todoList, task]); // вызывается сет тудулист чтобы поменять значение листа и добавить задание
+      setTodoList([...todoList, task]);
     };
 
-    const deleteTask = (id) => { // функция удаления задания, принимает айди
-        setTodoList(todoList.filter((task) => task.id !== id)); // метод фильтр проходит по каждому элементу
-        // списка тудулист и возвращает новый, оставляя только те которые прошли проверку, тоесть задача оставляется
-        // если ее id не равен данному айди который передался, функция записывает новый массив в стейт без айди который преедался
+    const deleteTask = (id) => {
+        setTodoList(todoList.filter((task) => task.id !== id));
     }
 
-    const completeTask = (id) => { // функция принимает айди, меняет булеан
+    const completeTask = (id) => {
         setTodoList(todoList.map((task) => task.id === id ? { ...task, isCompleted: !task.isCompleted } : task));
-        // метод мап проходит по каждому и меняет таск если проходит проверку, если эта та самя задача которую кликнули, если нет
-        // то возвращаем просто task, если да то создаем копию задачи но меняем будеан и сет туду лист записывает новый массив
     }
 
   return (
     <div className="App">
         <div className="addTask">
-            <input onChange={handleChange} type="text"/> // чтобы передать данные
-            <button onClick={addTask}>Add Task</button> // вызывается функция добавления
+            <input onChange={handleChange} type="text"/>
+            <button onClick={addTask}>Add Task</button>
         </div>
         <div className="list">
-            {todoList.map((task) => { // показывается весь список, метод мап проходит по каждому заданию
-                return <Task // компонент передаются все нужные данные, в том числе и функции
+            {todoList.map((task) => {
+                return <Task
                     taskName={task.taskName}
                     id={task.id}
                     deleteTask={deleteTask}
--- src/task.js
+++ src/task.js
@@ -1,9 +1,9 @@
-export const Task = (props) => { // функция таск, принимает пропс (аргументы, любые)
+export const Task = (props) => {
     return (
         <div>
-            <h1 style={{color: props.isCompleted ? "green" : "black"}}>{props.taskName}</h1> // Стиль, меняется цвет в зависимости от булеана исКосплетед
-            <button onClick={() => props.completeTask(props.id)}>Complete</button> // кнопка, при нажатии выполняется функция которая потом меняет булиан, передается айди
-            <button onClick={() => props.deleteTask(props.id)}>X</button> // кнопка при нажатии выполняется функция которя потом удаляет таск, передается также айди
+            <h1 style={{color: props.isCompleted ? "green" : "black"}}>{props.taskName}</h1>
+            <button onClick={() => props.completeTask(props.id)}>Complete</button>
+            <button onClick={() => props.deleteTask(props.id)}>X</button>
         </div>
     )
 }
