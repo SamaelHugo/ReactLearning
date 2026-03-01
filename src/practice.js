c348857997957dca75d433fbd2033dbdd2f4a305
SamaelHugo <aubekirov.2@gmail.com>
2026-03-01 15:17:50 +0500

    Повторение мать учения #2

--- src/practice.js
+++ src/practice.js
@@ -1,37 +1,52 @@
 import {useState} from "react";
 
-const [taskList, setTaskList] = useState();
-const [newTask  , setNewTask] = useState();
+const [task, setTask] =  useState("")
+const [taskList, setTaskList] = useState([])
 
-
-const setTask = (e) => {
-    setNewTask(e.target.value)
+const handleChange = (e) => {
+    setTask(e.target.value)
 }
 
 const addTask = () => {
     const task = {
         id: taskList.length === 0 ? 1 : taskList[taskList.length - 1].id + 1,
-        taskName: newTask,
+        taskName: task,
         isCompleted: false
     }
+
+    setTaskList([...taskList, task])
 }
 
 const deleteTask = (id) => {
-    setTaskList(taskList.filter((task) => task.id !== id))
+    setTaskList(taskList.filter((task) => {task.id !== id}))
 }
 
 const completeTask = (id) => {
-    setTaskList(taskList.map((task) => task.id === id ? {...task, isCompleted: !task.isCompleted} : task));
+    setTaskList(taskList.map((task) => task.id === id ? { ...task, isCompleted: !task.isCompleted } : task))
 }
 
 
+<div>
+    <input onChange={handleChange} type="text"/>
+    <button onClick={addTask} >Add task</button>
+    <div>
+        {taskList.map(task) => {
+            return <Task
+                taskName = {task.taskName}
+                id = {task.id}
+                completeTask = {completeTask}
+                deleteTask = {deleteTask}
+                isCompleted = {task.isCompleted}
+            />
+        }}
+    </div>
+</div>
+
 
 const Task = (props) => {
-    return (
-        <div>
-            <h1 style={{color: props.isCompleted ? "green" : "black"}} >{props.taskName}</h1>
-            <button onClick={() => {props.completeTask(props.id)}} >Complete</button>
-            <button onClick={() => {props.deleteTask(props.id)}} >X</button>
-        </div>
-    )
+    <div>
+        <h1 style={{color: props.isCompleted ? "green" : "black"}} >{props.taskName}</h1>
+        <button onClick={props.completeTask(props.id)} >Complete</button>
+        <button onClick={props.deleteTask(props.id)} >X</button>
+    </div>
 }
