b5cf1e5060dc9acbba10729941eadccf4970e0fa
SamaelHugo <aubekirov.2@gmail.com>
2026-03-07 01:45:03 +0500

    Создание уникального хука

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import {Form} from "./components/form";
+import {useToggle} from "./useToggle";
 
 
 function App() {
 
-
+    const [isVisible, toggle] = useToggle()
+    const [isVisible2, toggle2] = useToggle()
 
   return (
       <div className="App">
-          <Form />
+        <button onClick={toggle} >
+            {isVisible ? "Hide" : "Show"}
+        </button>
+          {isVisible && <h1>Hidden text</h1>}
+
+
+          <button onClick={toggle2} >
+              {isVisible2 ? "Hide" : "Show"}
+          </button>
+          {isVisible2 && <h1>Hidden text</h1>}
       </div>
   )
 }
