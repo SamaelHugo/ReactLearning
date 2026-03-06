27f93038126e07816922679b6777b97231ea7306
SamaelHugo <aubekirov.2@gmail.com>
2026-03-07 02:07:41 +0500

    Создание уникального хука для калькулятора, практика

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-import {Cat} from "./cat";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {useCount} from "./useCount";
 
 
 function App() {
 
-    const client = new QueryClient({
-        defaultOptions: {
-            queries: {
-                refetchOnWindowFocus: true,
-            }
-        }
-    })
+    const {count, increase, decrease, restart} = useCount();
 
   return (
       <div className="App">
-        <QueryClientProvider client={client}>
-             <Cat />
-        </QueryClientProvider>
+          {count}
+          <button onClick={increase} >Increase</button>
+          <button onClick={decrease} >Decrease</button>
+          <button onClick={restart} >Restart</button>
       </div>
   )
 }
